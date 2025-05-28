902c24ccfe981d68950fb3d63ce414bdbbdd6dd3
AKI-CHAUHAN123 <chauhanninja1234@gmail.com>
2025-05-28 15:35:59 +0530

    Add files via upload

---
+++ assignment_1/tutedude-assignment_1.py
@@ -0,0 +1,10 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number:"))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("Sum of no.:",c)
+print("Subtraction of number:",d)
+print("Multiplication of number:",e)
+print("Division of number:",f)
